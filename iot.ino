c70f6ad37ed6a1f4475cc8b3e1c31899a3f330d0
Fauzan <apauaie@gmail.com>
2023-08-15 15:17:32 +0800

    Initial Commit

---
+++ iot.ino
@@ -0,0 +1,83 @@
+#include <MakerKit.h>
+#include <ESP32Servo.h>
+
+#define BLYNK_TEMPLATE_ID           "TMPL6FQgF-y2Z"
+#define BLYNK_DEVICE_NAME           "Iot"
+#define BLYNK_AUTH_TOKEN            "9qsMEq30jIDQNn4Hcw_E6jLV55w_98_g"
+
+
+#define BLYNK_PRINT Serial
+
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "GRANADA";
+char pass[] = "";
+#include <Adafruit_NeoPixel.h>
+ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(5, 4, NEO_GRB + NEO_KHZ800);
+
+BLYNK_WRITE(V0)
+{
+int pinValue = param.asInt();
+  if (pinValue == 1) {
+    pixels.setPixelColor(0, 0xff0000);
+    pixels.setPixelColor(1, 0xff0000);
+    pixels.setPixelColor(2, 0xff0000);
+    pixels.setPixelColor(3, 0xff0000);
+    pixels.setPixelColor(4, 0xff0000);
+    pixels.show();
+    pixels.show();
+
+  }
+}
+
+BLYNK_WRITE(V1)
+{
+int pinValue = param.asInt();
+  if (pinValue == 1) {
+    pixels.setPixelColor(0, 0x3333ff);
+    pixels.setPixelColor(1, 0x3333ff);
+    pixels.setPixelColor(2, 0x3333ff);
+    pixels.setPixelColor(3, 0x3333ff);
+    pixels.setPixelColor(4, 0x3366ff);
+    pixels.show();
+    pixels.show();
+
+  }
+}
+
+Servo servo_5;
+BlynkTimer Timer1;
+
+void Timer1_TimerEvent()
+{
+  Blynk.virtualWrite(V20, ultrasonic(12,27));
+  Blynk.virtualWrite(V21, analogRead(39));
+  if (ultrasonic(12,27) < 10) {
+    servo_5.write(90);
+
+  } else {
+    servo_5.write(0);
+
+  }
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
+  pixels.begin();
+
+  pinMode(39, INPUT);
+  servo_5.attach(5);
+  Timer1.setInterval(300, Timer1_TimerEvent);
+
+}
+
+void loop() {
+  Blynk.run();
+  Timer1.run();
+}
